1a537b8a8bbc6291cc108f1e72872d84a192298d
forticolers <julien.jeanbourquin@hotmail.com>
2022-04-18 03:54:27 +0200

    feat: préparation du système de travail + préparation du projet java

---
+++ MurderAndMystery/src/test/java/LolTest.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jeanbourquj
+ */
+public class LolTest {
+    
+    public LolTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of toString method, of class Lol.
+     */
+    @Test
+    public void testToString() {
+        System.out.println("toString");
+       Lol l = new Lol("sesadsad");
+       System.out.println(l.toString());
+    }
+    
+}
